85e1bd17c7729291a91ba4b673d26b0b5908cb6d
Jim Gorman <jgorman30@gatech.edu>
2018-07-21 14:08:59 -0400

    Added a picture to the home page.

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -5,10 +5,25 @@ import {Labels} from '../../constants';
 class HomePage extends React.Component {
   render() {
     return (
-      <div>
-        <h1>{Labels.app_title}</h1>
-        <p>{Labels.app_description}</p>
-        <Link to="about" className="btn btn-primary btn-lg">{Labels.home.learn_more}</Link>
+      <div className="container">
+        <div className="row mb-1">
+          <div className="col-md-12">
+            <h1>{Labels.app_title}</h1>
+          </div>
+        </div>
+        <div className="row mb-4">
+          <div className="col-md-5">
+            <img src="../../images/teacher_teaching_kids_how_to_read.jpg" />
+          </div>
+          <div className="col-md-7">
+            {Labels.app_description}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <Link to="about" className="btn btn-primary btn-lg">{Labels.home.learn_more}</Link>
+          </div>
+        </div>
       </div>
     );
   }
