4d7bd532de955d15146c155433ff2a0fb34a8757
Jim Gorman <jgorman30@gatech.edu>
2018-07-28 10:38:22 -0400

    Made the YouTube videos optional.

--- src/components/studentLesson/StudentLessonPage.js
+++ src/components/studentLesson/StudentLessonPage.js
@@ -110,14 +110,14 @@ class StudentLessonPage extends React.Component {
     const steps =
       [
         {name: Labels.student.wizard_steps.welcome.title, component: <WelcomeWizardStep bookTitle={lesson.bookTitle} bookAmazonUrl={lesson.bookAmazonUrl} />},
-        {name: Labels.student.wizard_steps.the_hook.title, component: <YouTubeVideoWizardStep youTubeVideo={lesson.theHookYouTubeVideo} />},
-        {name: Labels.student.wizard_steps.two_vocabulary_words.title, component: <YouTubeVideoWizardStep youTubeVideo={lesson.theTwoVocabularyWordsYouTubeVideo} />},
+        ... lesson.theHookYouTubeVideo.url ? {name: Labels.student.wizard_steps.the_hook.title, component: <YouTubeVideoWizardStep youTubeVideo={lesson.theHookYouTubeVideo} />} : [],
+        ... lesson.theTwoVocabularyWordsYouTubeVideo.url ? {name: Labels.student.wizard_steps.two_vocabulary_words.title, component: <YouTubeVideoWizardStep youTubeVideo={lesson.theTwoVocabularyWordsYouTubeVideo} />} : [],
         {name: Labels.student.wizard_steps.please_read_the_book.title, component: <PleaseReadTheBookWizardStep bookTitle={lesson.bookTitle} bookAmazonUrl={lesson.bookAmazonUrl} />},
         {name: Labels.student.wizard_steps.card_pyramid.title, component: <CardPyramidWizardStep mainIdea={lesson.mainIdea} supportingIdea={lesson.supportingIdea} storyDetails={lesson.storyDetails} onChange={this.updateStudentLessonAnswers} answer={studentLessonAnswers.find(sla => sla.questionType === QuestionTypeEnum.card_pyramid)} />},
         {name: Labels.student.wizard_steps.word_scramble.title, component: <WordScrambleWizardStep onChange={this.updateStudentLessonAnswers} sentences={lesson.importantSentencesForWordScramble} answer={studentLessonAnswers.find(sla => sla.questionType === QuestionTypeEnum.word_scramble)} />},
         {name: Labels.student.wizard_steps.story_questions.title, component: <StoryQuestionsWizardStep questions={lesson.storyQuestions} answers={studentLessonAnswers.slice().filter(sla => sla.questionType === QuestionTypeEnum.story_question).sort((a, b) => { return a.questionId - b.questionId})} onChange={this.updateStudentLessonAnswers} />},
-        {name: Labels.student.wizard_steps.enunciation_video_1.title, component: <YouTubeVideoWizardStep explanationText={Labels.student.wizard_steps.enunciation_video_1.explanation} youTubeVideo={lesson.enunciationVideo1} />},
-        {name: Labels.student.wizard_steps.enunciation_video_2.title, component: <YouTubeVideoWizardStep explanationText={Labels.student.wizard_steps.enunciation_video_2.explanation} youTubeVideo={lesson.enunciationVideo2} />},
+        ... lesson.enunciationVideo1.url ? {name: Labels.student.wizard_steps.enunciation_video_1.title, component: <YouTubeVideoWizardStep explanationText={Labels.student.wizard_steps.enunciation_video_1.explanation} youTubeVideo={lesson.enunciationVideo1} />} : [],
+        ... lesson.enunciationVideo2.url ? {name: Labels.student.wizard_steps.enunciation_video_2.title, component: <YouTubeVideoWizardStep explanationText={Labels.student.wizard_steps.enunciation_video_2.explanation} youTubeVideo={lesson.enunciationVideo2} />} : [],
         {name: Labels.student.wizard_steps.congratulations.title, component: <CongratulationsWizardStep bookTitle={lesson.bookTitle} lessonAuthor={lesson.lessonAuthor} />}
       ];
 
